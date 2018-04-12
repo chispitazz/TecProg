d0830c1417b424602a98fdb190f8fe1360ab079a
Deivion <736520@unizar.es>
2018-04-12 10:56:03 +0200

    patata

---
+++ Nodo.java
@@ -0,0 +1,19 @@
+import java.lang.Iterable;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.lang.UnsupportedOperationException;
+
+abstract class NODO{
+		String nombreNodo;
+		abstract int size; 			// Solo para Fichero y Directorio
+
+		public NODO( String nombre){
+			nombreNodo= nombreNodo;
+		}
+		public void setNombre(String nombre){
+			nombreNodo=nombreNodo;
+		}
+		public String getNombre(){
+			
+		}
+}
